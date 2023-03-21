187874ee77136f70900197c9b0a035a91b8f1580
Steven Borisko <borisko@google.com>
2023-03-21 20:01:16 +0000

    rename some tests

--- test/aiinfra-cluster/terraform.sh
+++ test/aiinfra-cluster/terraform.sh
@@ -1,33 +1,33 @@
 . ./test/helpers.sh
 
-terraform::test::src_dir () {
+aiinfra-cluster::test::src_dir () {
     echo '/usr/primary'
 }
 
-terraform::test::data_dir () {
+aiinfra-cluster::test::data_dir () {
     # needs to be PWD because of the -chdir in the terraform commands
     echo "${PWD}/test/aiinfra-cluster/data"
 }
 
 # Helper functions
 
-terraform::test::plan () {
+aiinfra-cluster::test::plan () {
     local -r var_file="${1}"
     local -r tmp_file="${2}"
     ! jq -e 'select(."@level" == "error")' >&2 \
-        <(terraform -chdir="$(terraform::test::src_dir)" plan \
+        <(terraform -chdir="$(aiinfra-cluster::test::src_dir)" plan \
             -no-color -json -parallelism=$(nproc) -lock=false \
             -var-file="${var_file}" -out="${tmp_file}" \
         && echo '{"@level":"info","@message":"success"}')
 }
 
-terraform::test::show () {
+aiinfra-cluster::test::show () {
     local -r tmp_file="${1}"
-    terraform -chdir="$(terraform::test::src_dir)" show \
+    terraform -chdir="$(aiinfra-cluster::test::src_dir)" show \
         -no-color -json "${tmp_file}"
 }
 
-terraform::test::json_contains () {
+aiinfra-cluster::test::json_contains () {
     element_file="${1}"
     input_file="${2}"
     # reference for `contains(element)` function:
@@ -44,32 +44,36 @@ skip::test::aiinfra-cluster::fmt () {
 
 test::aiinfra-cluster::passes_on_default_vars () {
     tfplan=$(mktemp)
-    EXPECT_SUCCEED terraform::test::plan \
-        "$(terraform::test::data_dir)/passes_on_default_vars.tfvars" \
+    EXPECT_SUCCEED aiinfra-cluster::test::plan \
+        "$(aiinfra-cluster::test::data_dir)/passes_on_default_vars.tfvars" \
         "${tfplan}"
-    EXPECT_SUCCEED terraform::test::json_contains \
-        "$(terraform::test::data_dir)/passes_on_default_vars.json" \
-        <(terraform::test::show "${tfplan}")
+    EXPECT_SUCCEED aiinfra-cluster::test::json_contains \
+        "$(aiinfra-cluster::test::data_dir)/passes_on_default_vars.json" \
+        <(aiinfra-cluster::test::show "${tfplan}")
 }
 
 test::aiinfra-cluster::fails_on_empty_vars () {
-    EXPECT_FAIL terraform::test::plan \
-        "$(terraform::test::data_dir)/fails_on_empty_vars.tfvars" \
+    EXPECT_FAIL aiinfra-cluster::test::plan \
+        "$(aiinfra-cluster::test::data_dir)/fails_on_empty_vars.tfvars" \
         "$(mktemp)" 2>/dev/null
     #| jq 'if ."@level" == "error" then halt_error else empty end'
 }
 
-skip::test::aiinfra-cluster::disable_ops_agent_removes_dashboard () {
+skip::test::aiinfra-cluster::dashboard::is_removed_when_disable_ops_agent () {
     tfplan=$(mktemp)
-    EXPECT_SUCCEED terraform::test::plan \
-        "$(terraform::test::data_dir)/disable_ops_agent_removes_dashboard.tfvars" \
+    EXPECT_SUCCEED aiinfra-cluster::test::plan \
+        "$(aiinfra-cluster::test::data_dir)/disable_ops_agent_removes_dashboard.tfvars" \
         "${tfplan}"
     tfshow=$(mktemp)
-    terraform::test::show "${tfplan}" >"${tfshow}"
-    EXPECT_SUCCEED terraform::test::json_contains \
-        "$(terraform::test::data_dir)/disable_ops_agent_has_compute.json" \
+    aiinfra-cluster::test::show "${tfplan}" >"${tfshow}"
+    EXPECT_SUCCEED aiinfra-cluster::test::json_contains \
+        "$(aiinfra-cluster::test::data_dir)/disable_ops_agent_has_compute.json" \
         "${tfshow}"
-    EXPECT_FAIL terraform::test::json_contains \
-        "$(terraform::test::data_dir)/disable_ops_agent_removes_dashboard.json" \
+    EXPECT_FAIL aiinfra-cluster::test::json_contains \
+        "$(aiinfra-cluster::test::data_dir)/disable_ops_agent_removes_dashboard.json" \
         "${tfshow}"
+}
+
+skip::test::aiinfra-cluster::slurm-controller::gets_created_when_orchestrator_set_to_slurm () {
+    false
 }
