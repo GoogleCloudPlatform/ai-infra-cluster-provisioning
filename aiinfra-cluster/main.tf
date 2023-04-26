cb8a9478a2d32c439c214f1eda73b8d7c1290d41
Steven Borisko <borisko@google.com>
2023-04-26 09:24:56 -0700

    add gcsfuse_mount to network storage

--- aiinfra-cluster/main.tf
+++ aiinfra-cluster/main.tf
@@ -182,7 +182,10 @@ module "aiinfra-compute" {
   orchestrator_type            = var.orchestrator_type
   slurm_node_count_static      = var.slurm_node_count_static
   slurm_node_count_dynamic_max = var.slurm_node_count_dynamic_max
-  network_storage              = flatten([module.nfs_filestore[*].network_storage])
+  network_storage              = flatten([
+    module.nfs_filestore[*].network_storage,
+    module.gcsfuse_mount[*].network_storage,
+  ])
   gke_version                  = var.gke_version
   
   node_pools = length(var.custom_node_pool) != 0 || length(local.basic_node_pool) != 0 ? coalescelist(var.custom_node_pool, local.basic_node_pool) : []
