7525bc71d7e1dd276bcd2c202fe2f3393478f7c7
soumyapani <112522451+soumyapani@users.noreply.github.com>
2023-04-26 16:19:25 -0700

    fixing service account to GKE module.

--- aiinfra-cluster/variables.tf
+++ aiinfra-cluster/variables.tf
@@ -32,8 +32,8 @@ variable "service_account" {
       "https://www.googleapis.com/auth/monitoring.write",
       "https://www.googleapis.com/auth/servicecontrol",
       "https://www.googleapis.com/auth/service.management.readonly",
-    "https://www.googleapis.com/auth/trace.append",
-    "cloud-platform"]
+      "https://www.googleapis.com/auth/trace.append",
+      "cloud-platform"]
   }
 }
 
