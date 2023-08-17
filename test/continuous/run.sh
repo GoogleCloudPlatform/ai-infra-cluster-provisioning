7ca5071e84c7bae70c269f33559214425059efa8
Steven Borisko <borisko@google.com>
2023-08-17 11:37:56 -0700

    remove infeasible tests

--- test/continuous/run.sh
+++ test/continuous/run.sh
@@ -6,6 +6,7 @@
 . ./test/continuous/a3/terraform/modules/cluster/gke-beta/tests.sh
 . ./test/continuous/a3/terraform/modules/cluster/mig/tests.sh
 . ./test/continuous/a3/terraform/modules/cluster/mig-cos/tests.sh
-. ./test/continuous/a3/terraform/modules/cluster/slurm/tests.sh
+# Removing until slurm actually works on `a3-highgpu-8g`
+#. ./test/continuous/a3/terraform/modules/cluster/slurm/tests.sh
 
 runner::main "${@}"
