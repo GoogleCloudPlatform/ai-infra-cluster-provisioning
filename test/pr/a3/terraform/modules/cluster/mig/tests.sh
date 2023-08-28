4fe889a904fbf921f677bcba75c1a0fa4fc84aed
Soumya Pani <soumyapani@google.com>
2023-08-28 12:11:00 -0700

    Adding machine type per instance_groups in MIG.

--- test/pr/a3/terraform/modules/cluster/mig/tests.sh
+++ test/pr/a3/terraform/modules/cluster/mig/tests.sh
@@ -32,3 +32,20 @@ test::a3::terraform::mig::simple_create_modules () {
         "$(a3::terraform::mig::output_dir)/modules.json" \
         "${tfshow}"
 }
+
+test::a3::terraform::mig::multiple_create_modules () {
+    local -r tfvars=$(mktemp)
+    helpers::append_tfvars "$(a3::terraform::mig::input_dir)/multi.tfvars" mig >"${tfvars}"
+
+    local -r tfplan=$(mktemp)
+    EXPECT_SUCCEED helpers::terraform_plan \
+        "$(a3::terraform::mig::src_dir)" \
+        "${tfvars}" \
+        "${tfplan}"
+
+    local -r tfshow=$(mktemp)
+    helpers::terraform_show "$(a3::terraform::mig::src_dir)" "${tfplan}" >"${tfshow}"
+    EXPECT_SUCCEED helpers::json_contains \
+        "$(a3::terraform::mig::output_dir)/multimodules.json" \
+        "${tfshow}"
+}
