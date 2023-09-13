ac9a475b92b486cf709441684f7a91d60af7ef17
Soumya Pani <soumyapani@google.com>
2023-09-13 10:02:25 -0700

    diabling gke-beta tests for A3.

--- test/continuous/run.sh
+++ test/continuous/run.sh
@@ -3,7 +3,8 @@
 . ./test/runner.sh
 
 . ./test/continuous/a3/terraform/modules/cluster/gke/tests.sh
-. ./test/continuous/a3/terraform/modules/cluster/gke-beta/tests.sh
+# removing gke-beta tests for A3 until we have A3 capacity available for testing.
+# . ./test/continuous/a3/terraform/modules/cluster/gke-beta/tests.sh
 . ./test/continuous/a3/terraform/modules/cluster/mig/tests.sh
 . ./test/continuous/a3/terraform/modules/cluster/mig-cos/tests.sh
 # Removing until slurm actually works on `a3-highgpu-8g`
